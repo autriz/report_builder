305d6f36bb4e0ca0e2a1ea90c8722c3ed8343b79
Leonid-Yakovlev63 <j772115@gmail.com>
2024-10-26 22:42:27 +0300

    Added countByDate to DateAnalyzer

--- src/lib/server/processing/calculations/DataAnalyzer.ts
+++ src/lib/server/processing/calculations/DataAnalyzer.ts
@@ -21,6 +21,21 @@ class DataAnalyzer {
         return total / count;
     }
 
+    //[{ a: 10, b: 5, c: '50', value: 'value', date: new Date('2023-10-26') }]
+    countByDate(data: ResultRow[], dateKey: string): Map<string, number> {
+        const dateCountMap = new Map<string, number>();
+    
+        data.forEach(row => {
+            const date = row[dateKey];
+            if (date instanceof Date) {
+                const formattedDate = date.toISOString().split('T')[0]; // YYYY-MM-DD
+                dateCountMap.set(formattedDate, (dateCountMap.get(formattedDate) || 0) + 1);
+            }
+        });
+    
+        return dateCountMap;
+    }
+    
 }
 
 export default new DataAnalyzer();
